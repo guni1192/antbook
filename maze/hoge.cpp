28e84755268df5292ad7e0ef4a392aaba03eb19a
Takashi IIGUNI <iiguni.tks@gmail.com>
2020-03-18 17:39:32 +0900

    remove debugging code

--- maze/hoge.cpp
+++
@@ -1,9 +0,0 @@
-#include <bits/stdc++.h>
-
-int main(void) {
-    std::string str1 = "hello";
-    std::string str2 = str1;
-
-    std::cout << str1 << std::endl;
-    std::cout << str2 << std::endl;
-}
