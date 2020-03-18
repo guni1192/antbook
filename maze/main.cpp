15d6b903a0e26224c56112cde5a5f944cb691db2
Takashi IIGUNI <iiguni.tks@gmail.com>
2020-03-18 18:35:12 +0900

    implement bfs

--- maze/main.cpp
+++ maze/main.cpp
@@ -10,26 +10,10 @@ typedef pair<int, int> Point;
 
 Point find_point(const vector<string> maze, const char ch, const int n,
                  const int m);
-void debug(const vector<string> maze, const int n, const int m);
 void solve(const vector<string> maze, const int n, const int m);
 
 int bfs(const vector<string> maze, const int n, const int m, Point start,
         Point goal);
-
-void debug(const vector<string> maze, const int n, const int m) {
-    for (auto line : maze) {
-        cout << line << endl;
-    }
-
-    cout << "n: " << n << endl;
-    cout << "m: " << m << endl;
-
-    Point start = find_point(maze, 'S', n, m);
-    printf("Start: (%d, %d)", start.first, start.second);
-
-    Point goal = find_point(maze, 'G', n, m);
-    printf("Start: (%d, %d)", goal.first, goal.second);
-}
 
 Point find_point(const vector<string> maze, const char ch, const int n,
                  const int m) {
@@ -45,27 +29,33 @@ Point find_point(const vector<string> maze, const char ch, const int n,
 }
 int bfs(const vector<string> maze, const int n, const int m, Point start,
         Point goal) {
-    queue<Point> q;
-    int inf = std::numeric_limits<int>::infinity();
+    const int inf = std::numeric_limits<int>::infinity();
     vector<int> t(m, inf);
     vector<vector<int>> d(n, t);
+    queue<Point> q;
     q.push(start);
 
     while (q.size()) {
         Point p = q.front();
         q.pop();
         if (p == goal) break;
-    }
 
-    for (int i = 0; i < 4; i++) {
-        int nx = p.first + dx[i], ny = p.second + dy[i];
+        for (int i = 0; i < 4; i++) {
+            int nx = p.first + dx[i], ny = p.second + dy[i];
+            if (0 <= nx && nx < n && 0 <= ny && ny < m && maze[nx][ny] != '#' &&
+                d[nx][ny] == inf) {
+                q.push(Point(nx, ny));
+                d[nx][ny] = d[p.first][p.second] + 1;
+            }
+        }
     }
+    return d[goal.first][goal.second];
 }
 
 void solve(const vector<string> maze, const int n, const int m) {
-    debug(maze, n, m);
     Point start = find_point(maze, 'S', n, m);
     Point goal = find_point(maze, 'G', n, m);
+
     cout << bfs(maze, n, m, start, goal) << endl;
 }
 
